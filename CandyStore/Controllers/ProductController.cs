ffe2750cc2f5943eabae69692d5d8978873b492f
hoppenic <hoppenic@gmail.com>
2018-07-09 11:41:20 -0500

    update candystore

---
+++ CandyStore/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyStore.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private List<Product> _products;
+
+
+        //this is a constructor
+        public ProductController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
