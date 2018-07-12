7fe20fb76c4783762fd42f113b864d9eb3609a82
hoppenic <hoppenic@gmail.com>
2018-07-12 11:00:39 -0500

    update

---
+++ CandyStore/Models/CandyStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandyStore.Models
+{
+    public class CandyStoreDbContext
+    {
+
+
+    }
+}
